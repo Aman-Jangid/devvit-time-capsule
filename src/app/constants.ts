7da6c64b9b460a5ac60ecc218bcbeb79d8e9ac15
Aman-Jangid <aman2003jangid@gmail.com>
2025-03-16 03:48:20 +0530

    Create final layout

---
+++ src/app/constants.ts
@@ -0,0 +1,9 @@
+export const MAIN_SUBREDDIT = "testmygame098";
+// export const
+export const FLAIRS = [
+  "Prediction",
+  "Announcement",
+  "Nostalgia",
+  "Challenge",
+  "Other",
+];
--- src/main.tsx
+++ src/main.tsx
@@ -32,8 +32,12 @@ Devvit.addMenuItem({
       subredditName: subreddit.name,
       preview: (
         <vstack height="100%" width="100%" alignment="middle center">
-          <image url="logo.png" imageHeight={300} imageWidth={300} />
-          <text size="large">🛠️ Creating time capsule...just a moment. </text>
+          <image
+            url="logo.png"
+            imageHeight={300}
+            imageWidth={300}
+            resizeMode="cover"
+          />
         </vstack>
       ),
     });
@@ -41,6 +45,7 @@ Devvit.addMenuItem({
   },
 });
 
+// Main post type for the time capsule
 Devvit.addCustomPostType({
   name: "timeCapsule",
   height: "regular",
@@ -54,10 +59,16 @@ Devvit.addCustomPostType({
       theme: [""],
     });
     const [backgroundUrl, setBackgroundUrl] = useState("background.jpg");
+    const [error, setError] = useState("");
+
+    const CURRENT_SUBREDDIT_NAME = context.subredditName || "";
+    const CURRENT_POST_ID = context.postId || "";
 
     const form = useForm(
       {
-        title: "Time capsule creator",
+        acceptLabel: "Bury Capsule",
+        cancelLabel: "Later",
+        title: "Create Time Capsule",
         fields: [
           {
             name: "capsuleTitle",
@@ -74,11 +85,27 @@ Devvit.addCustomPostType({
             defaultValue: formContent.description,
           },
           {
-            name: "revealDate",
+            name: "capsuleRevealDate",
             type: "string",
             label: "Reveal Date",
+            helpText: "Format: MM/DD/YYYY::HH:MM AM/PM",
+            placeholder: "12/12/2025::12:00 AM",
             required: true,
             defaultValue: formContent.revealDate,
+          },
+          {
+            name: "capsuleTheme",
+            type: "select",
+            label: "Type of content inside the capsule",
+            options: [
+              { label: "Prediction", value: "Prediction" },
+              { label: "Announcement", value: "Announcement" },
+              { label: "Nostalgia", value: "Nostalgia" },
+              { label: "Challenge", value: "Challenge" },
+              { label: "Other", value: "Other" },
+            ],
+            required: true,
+            defaultValue: formContent.theme,
           },
           {
             name: "capsuleImage",
@@ -86,38 +113,29 @@ Devvit.addCustomPostType({
             label: "Image (optional)",
             required: false,
           },
-          {
-            name: "capsuleTheme",
-            type: "select",
-            label: "Theme",
-            options: [
-              { label: "General", value: "General" },
-              { label: "Prediction", value: "Prediction" },
-              { label: "Advice", value: "Advice" },
-              { label: "Announcement", value: "Announcement" },
-              { label: "Story", value: "Story" },
-              { label: "Challenge", value: "Challenge" },
-              { label: "Other", value: "Other" },
-            ],
-            required: true,
-            defaultValue: formContent.theme,
-          },
         ],
       },
-      ({
-        capsuleTitle,
-        capsuleDescription,
-        capsuleImage,
-        capsuleTheme,
-        revealDate,
-      }) => {
+      async (data) => {
         setFormContent({
-          title: capsuleTitle,
-          description: capsuleDescription,
-          revealDate: revealDate,
-          image: capsuleImage,
-          theme: capsuleTheme,
+          title: data.capsuleTitle,
+          description: data.capsuleDescription,
+          revealDate: data.capsuleRevealDate,
+          image: data.capsuleImage,
+          theme: data.capsuleTheme,
         });
+
+        // get post id
+        const capsuleId = Date.now().toString();
+        await context.redis.set(capsuleId, JSON.stringify(data), {});
+        // create post
+        try {
+        } catch (e) {
+          setError(e as any); // setError(e); later - display on error page
+          setPage(4);
+
+          return;
+        }
+
         setPage(1);
       }
     );
@@ -141,16 +159,26 @@ Devvit.addCustomPostType({
       case 0:
         content = (
           <>
-            <image
-              url="background.jpg"
-              imageHeight={500}
-              imageWidth={500}
-              height="100%"
+            <hstack
               width="100%"
-              resizeMode="cover"
-            />
+              height="100%"
+              alignment="top start"
+              padding="medium"
+            >
+              <text size="medium" weight="bold">
+                {`<Time Capsule ~ ${CURRENT_SUBREDDIT_NAME}>`}
+              </text>
+            </hstack>
+            <hstack
+              width="100%"
+              height="100%"
+              alignment="top end"
+              padding="medium"
+            >
+              <icon name="help" size="large" onPress={() => setPage(5)} />
+            </hstack>
             <vstack height="100%" width="100%" alignment="middle center">
-              <button onPress={() => context.ui.showForm(form)}>
+              <button onPress={() => context.ui.showForm(form)} size="large">
                 Create time capsule
               </button>
             </vstack>
@@ -162,15 +190,27 @@ Devvit.addCustomPostType({
         content = (
           <>
             <vstack height="100%" width="100%" alignment="middle center">
-              <text size="large">
+              <text size="xlarge" weight="bold" wrap>
                 Are you sure about the capsule's content?
               </text>
-              <spacer height={5} />
+              <hstack grow={false} width="100%" alignment="middle center">
+                <text size="large" weight="bold">
+                  {"( Creating in  "}
+                </text>
+                <text size="large" color="orange" weight="bold">
+                  {CURRENT_SUBREDDIT_NAME}
+                </text>
+                <text size="large" weight="bold">
+                  {"  subreddit. )"}
+                </text>
+              </hstack>
+              <spacer height={12} />
               <button onPress={() => buryCapsule()}>Yes, Bury Capsule</button>
+              <spacer height={8} />
               <button onPress={() => context.ui.showForm(form)}>
                 No, Edit Content..
               </button>
-              <spacer height={5} />
+              <spacer height={12} />
             </vstack>
           </>
         ) as any;
@@ -179,45 +219,87 @@ Devvit.addCustomPostType({
       case 2:
         content = (
           <vstack height="100%" width="100%" alignment="middle center">
-            <text size="large">Burying your time capsule....</text>
+            <text size="xlarge">Burying your time capsule....</text>
           </vstack>
         ) as any;
         break;
-      // 3: Success message
+      // 3: Success
       case 3:
         content = (
-          <vstack height="100%" width="100%" alignment="middle center">
-            <text size="large">Your time capsule was successfully buried!</text>
-            <spacer height={5} />
-            <text size="medium">
-              It will be revealed to the @community on{" "}
-              <text weight="bold" size="large">
-                12/12/2025
-              </text>
-              ,
+          <vstack
+            height="100%"
+            width="100%"
+            alignment="middle center"
+            padding="small"
+          >
+            <text size="xxlarge" wrap>
+              Your time capsule was successfully buried!
             </text>
-            <text size="medium">
+            <spacer height={15} />
+            <text size="xlarge" weight="bold" wrap>
+              It will be revealed to the {CURRENT_SUBREDDIT_NAME} on
+            </text>
+            <text weight="bold" size="xlarge" color="orange">
+              {" " + formContent.revealDate + " "}
+            </text>
+            <spacer height={5} />
+            <text size="xlarge" wrap>
               You will be notified 10 minutes before the reveal.
             </text>
-            <spacer height={10} />
-            <button onPress={() => context.ui.navigateTo("")}>
+            <spacer height={20} />
+            <button
+              onPress={() =>
+                context.ui.navigateTo(
+                  `https://www.reddit.com/r/${CURRENT_SUBREDDIT_NAME}/comments/${CURRENT_POST_ID}/`
+                )
+              }
+            >
               See your post {"->"}
             </button>
           </vstack>
         ) as any;
         break;
-      // 4: Error message
+      // 4: Error
       case 4:
         content = (
           <vstack height="100%" width="100%" alignment="middle center">
-            <text size="large">
+            <text size="xlarge">
               We are facing some issues creating your time capsule please check
               back a few minutes later. {":("}
             </text>
-            <button onPress={() => setPage(0)}>Okay :|</button>
+            <spacer height={8} />
+            <text size="xlarge">{error}</text>
+            <spacer height={10} />
+            <button onPress={() => buryCapsule()}>Retry</button>
+            <spacer height={8} />
+            <button onPress={() => setPage(0)}>Go Back</button>
           </vstack>
         ) as any;
         break;
+      // 4: About
+      case 5:
+        content = (
+          <vstack height="100%" width="100%" alignment="middle center">
+            <vstack height="100%" width="60%" alignment="middle center">
+              <text size="xxlarge">Time Capsule</text>
+              <spacer height={10} />
+              <text size="xlarge" wrap alignment="middle center">
+                Create a time capsule to be revealed in the future.
+              </text>
+              <spacer height={5} />
+              <text size="xlarge" wrap alignment="middle center">
+                The time capsule will be buried in the subreddit you are
+                currently in and will be revealed on the date you set.
+              </text>
+              <spacer height={5} />
+              <text size="xlarge" wrap alignment="middle center">
+                You will be notified 10 minutes before the reveal.
+              </text>
+              <spacer height={10} />
+              <button onPress={() => setPage(0)}>Back</button>
+            </vstack>
+          </vstack>
+        ) as any;
     }
 
     return (
@@ -230,6 +312,7 @@ Devvit.addCustomPostType({
           height="100%"
           width="100%"
         />
+
         {content}
       </zstack>
     );
